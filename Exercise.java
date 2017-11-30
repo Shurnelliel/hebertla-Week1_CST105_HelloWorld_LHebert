ff8fd4e279868bf899a0cbd1c39b3e70244ae44a
Shurnelliel <33744041+Shurnelliel@users.noreply.github.com>
2017-11-29 21:59:13 -0700

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,5 @@
+Enter a 5-digit positive interger: [43210]
+The sum of the digits is 4 + 3 + 2 + 1 + 0 = 10
+public class Exercise {
+
+}
---
+++ Exercise2.java
@@ -0,0 +1,7 @@
+Enter a Fahrenheit temperature: [107]
+107F is equivalent to 41.6667
+Enter a celius temperature: [107]
+100C is equivalent to 224.6F
+public class Exercise2 {
+
+}
